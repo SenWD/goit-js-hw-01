a546fbc890aa3b2a77e6ecdbf9dedd84af9381ac
SenWD <sengun.wd@gmail.com>
2024-12-16 07:02:19 +0300

    control app

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,26 @@
-function getElementWidth (content, padding, border) {
 
-   return `${Math.ceil(parseFloat(content)) + Math.ceil(2 * parseFloat(padding)) + Math.ceil(2 * parseFloat(border))}`;
+function checkForSpam(message) {
 
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+   message = message.toLowerCase(); 
+   
+    if  (message.includes("spam") || message.includes("sale")){ 
+  
+          return true; //console.log(true)
+      } else {
+  
+         return false; //console.log(false)
+      }
+      
+  } 
+  
+  
+  console.log(checkForSpam("Latest technology news")); // false
+  console.log(checkForSpam("JavaScript weekly newsletter")); // false
+  console.log(checkForSpam("Get best sale offers now!")); // true
+  console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+  console.log(checkForSpam("Trust me, this is not a spam message")); // true
+  console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+  console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+  
 
+  
